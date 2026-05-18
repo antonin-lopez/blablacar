48bf0fb59a34b93bba9334e4cfa30c4fbe4e7d64
Antoine Schaeffer <antoine.schaeffer35@orange.fr>
2026-05-18 21:24:19 +0200

    début menu

--- index.php
+++ index.php
@@ -1,5 +1,10 @@
 <?php
 session_start();
 $_SESSION['login_id'] = -1;
+$_SESSION['nom'] = "";
+$_SESSION['prenom'] = "";
+$_SESSION['role'] = "";
+$_SESSION['solde'] = 0;
 header('Location: app/router/router.php?action=truc');
+exit();
 ?>
