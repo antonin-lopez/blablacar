b23c00435570fc842a9338c2c57ca38f40007125
Antoine Schaeffer <antoine.schaeffer35@orange.fr>
2026-06-02 09:46:32 +0200

    list

--- app/view/innovation/viewInnovations.php
+++ app/view/innovation/viewInnovations.php
@@ -6,11 +6,13 @@
     <main class="container mt-5 d-flex flex-column gap-4">
         <h1>Amélioration du code MVC</h1>
         <div class="alert alert-info">
-            Le routeur a été modifié pour accepter deux paramètres d'entrée distincts (contrôleur et action), au lieu d'un seul auparavant. </p>
-            Le nom du contrôleur est désormais construit à partir du paramètre contrôleur, puis on vérifie l'existence de la méthode correspondant au paramètre action. </p>
-            Ainsi, les deux paramètres sont cités séparément dans le lien hypertexte de la navbar.
+            <ul>
+                <li>Le routeur a été modifié pour accepter deux paramètres d'entrée distincts (contrôleur et action), au lieu d'un seul auparavant.</li>
+                <li>Le nom du contrôleur est désormais construit à partir du paramètre contrôleur, puis on vérifie l'existence de la méthode correspondant au paramètre action.</li>
+                <li>Ainsi, les deux paramètres sont cités séparément dans le lien hypertexte de la navbar.</li>
+            </ul>
         </div>
-        
+
     </main>
 
     <?php require ROOT . '/app/view/partials/footer.html'; ?>
