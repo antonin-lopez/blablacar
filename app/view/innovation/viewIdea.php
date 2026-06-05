40acc0efd14497323c30624b6528decbb7abb564
Antoine Schaeffer <antoine.schaeffer35@orange.fr>
2026-06-05 14:30:49 +0200

    idee

--- app/view/innovation/viewIdea.php
+++ app/view/innovation/viewIdea.php
@@ -4,9 +4,13 @@
     <?php require ROOT . '/app/view/partials/navbar.php'; ?>
 
     <main class="container mt-5 d-flex flex-column gap-4">
-        <h1>Idée originale</h1>
-        
-        
+        <div class="alert alert-info">
+            <ul>
+                <li>Mise en place d'un système de niveaux en fonction de l'utilisation de BlaBlaCar</li>
+                <li>Circuit de la récompense | Dopamine | Gamification</li>
+                <li>Système innovant pour BlaBlaCar et d'autres entreprises d'un secteur différent ont prouvé le succès</li>
+            </ul>
+        </div>
     </main>
 
     <?php require ROOT . '/app/view/partials/footer.html'; ?>
